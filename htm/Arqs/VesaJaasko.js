69ddbf76ed729bdd5ccaee74a567a8dbe4138299
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:59 -0200

    Sent by DeployFC at 01/02/2017 20:41:59

---
+++ htm/Arqs/VesaJaasko.js
@@ -0,0 +1,9 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/DN_VesaJaasko.jpg" width="214" height="214" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Vesa Jääskö</span></p><palign="justify">O designer Finlandês Vesa Jääskö busca inspiração nos problemas ainda sem solução no ambiente humano de constantes transformações. Para ele, o design é sempre uma mistura de conhecimento e surpresa que faz com que o novo produto seja significativo para o usuário.Em 2006 fundou o estudo de design Vesavisio Oy. Seu trabalho inclui uma variada gama de produtos, que vai desde telefones celulares, passando por bicicletas, instrumentos industriais até utensílios para cozinha.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
