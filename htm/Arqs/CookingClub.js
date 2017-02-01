12f6120fe7ef1c78903baa7f81b428d886c745ea
designanyware <miriam@designanyware.com.br>
2017-02-01 20:40:19 -0200

    Sent by DeployFC at 01/02/2017 20:40:19

---
+++ htm/Arqs/CookingClub.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('   <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_CookingClub.gif" width="214" height="120" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Cooking Club of America</span></p>');
+document.write('      <p align="justify">O Cooking Club of America é um clube e uma revista sobre culinária, que inclui receitas, fóruns e testes de produtos. Iniciado em 1998 faz parte do North Amercian Membership Group, Inc. Tem uma comunidade ativa no facebook e um canal no YouTube.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
