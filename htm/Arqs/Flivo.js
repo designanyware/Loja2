d3ee261f99a3c981bbe867b4239475e4a7dba205
designanyware <miriam@designanyware.com.br>
2017-02-01 20:40:48 -0200

    Sent by DeployFC at 01/02/2017 20:40:48

---
+++ htm/Arqs/Flivo.js
@@ -0,0 +1,14 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/MA_Flivo.jpg" width="214" height="120" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Flivo</span></p>');
+document.write('     <p align="justify"> Flivo é uma marca brasileira com sede em São Paulo, fundada em 2012 pelos designers Caroline Duran e Jackson Oliveira. Fabrica produtos úteis e divertidos para casa e uso pessoal, com estampas originais, desenvolvidas a partir de ideias do cotidiano e tendências da decoração. São produtos de ótima qualidade, com materiais de primeira classe, perfeitos para presentear com originalidade ou decorar sua casa com muito estilo e personalidade.');
+document.write('      </p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
