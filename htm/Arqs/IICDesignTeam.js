91cbc59235cd8a510278765e30f52e8db14782db
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:15 -0200

    Sent by DeployFC at 01/02/2017 20:41:15

---
+++ htm/Arqs/IICDesignTeam.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/DN_IICDesign.gif" width="214" height="45" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">IIC Design Team</span></p>');
+document.write('      <p align="justify">Com mais de 20 de experiência no desenvolvimento de ideias para produtos de consumo, a International Innovation Company (IIC) é uma pioneira em inovação. Seu foco principal é produzir produtos inovadores, divertidos e práticos para consumidores em todo o mundo.</p><p align="justify"> Rui Medeiros Santos é atualmente o designer que lidera o time de desenvolvimento de produtos, cuidando da criação e conceituação de todos os novos produtos da marca Vacu Vin.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
