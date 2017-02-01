daf500127e455a3afe81423a8e5af76cdee2b3f6
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:02 -0200

    Sent by DeployFC at 01/02/2017 20:41:01

---
+++ htm/Arqs/GoodDAJapan.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>')
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_GoodDAJapan.gif" width="214" height="100" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Good Design Award (Japan)</span></p>');
+document.write('      <p align="justify">Também conhecido como “G Mark“, o prêmio concedido pelo JDP (Japan Institute of Design Promotion) é provavelmente o mais prestigiado do Japão. Tem suas origens no “Good Design Selection System”, instituído pelo Ministério do Comércio Internacional e Indústria em 1957, como uma medida política para promover o desenvolvimento da indústria, a partir da crença de que o design era essencial para romper o ciclo da pobreza no país.</p><p align="justify">Como cada vez mais empresas vêm incorporado o design nas atividades industriais, o prêmio começou a se expandir, abrangendo hoje uma ampla variedade de áreas como arquitetura, meio ambiente, comunicação e design. Inclui ainda projetos experimentais em áreas técnicas e de negócios que tenham design em seu núcleo.</p><p align="justify">Há mais de 50 anos, o Good Design Japan reconhece produtos que se diferenciam de seus contemporâneos. Sua importância transcende a simples avaliação do design e o concurso é considerado uma ferramenta social capaz de promover o desenvolvimento da indústria e elevar a qualidade de vida da população ao mesmo tempo.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
