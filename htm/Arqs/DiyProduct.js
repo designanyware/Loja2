0d6261fbead5d4011af5fe645896358d3936af06
designanyware <miriam@designanyware.com.br>
2017-02-01 20:40:39 -0200

    Sent by DeployFC at 01/02/2017 20:40:39

---
+++ htm/Arqs/DiyProduct.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>')
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_DiyProduct.gif" width="163" height="127" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Product of the Year Award</span></p>');
+document.write('    <p align="justify">A premiação anual “Product of the Year”, organizada pelas revistas DIY Week e Housewares, é a única no Reino Unido que cobre todo o espectro das utilidade domésticas.</p><p align="justify">Anteriormente conhecido como “Industry Awards”, o prêmio foi criado em 1960 pelo  “Hardware Trade Journal”, antecessor da revista DIY Week e foi relançado em 1988 com o nome “Product of the Year Awards”.</p><p align="justify">Todos os produtos inscritos são julgados por um juri independente, formado por varejistas que avaliam cada produto a partir de um conjunto predefinido de critérios. O principal objetivo é reconhecer a inovação, imaginação e criatividade da indústria, tanto no desenvolvimento de novas ideias quanto na reinvenção de antigos produtos.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
