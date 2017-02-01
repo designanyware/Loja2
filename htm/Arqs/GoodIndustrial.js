e9ea6e44c68ed58dae9e39149a4bb491108af410
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:05 -0200

    Sent by DeployFC at 01/02/2017 20:41:05

---
+++ htm/Arqs/GoodIndustrial.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_GoodIndustrial.gif" width="214" height="120" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Good Industrial Design</span></p>');
+document.write('      <p align="justify">O Good Industrial Design, mais conhecido como GIO (Goed Industrieel Ontwerp), é um prêmio Holandês de design, lançado em 1986 pela Link Design, com o objetivo de incentivar e promover o desenvolvimento e a profissionalização do design industrial na Holanda. A premiação acontece anualmente durante a Dutch Design Week (Semana de Design Holandesa), na cidade de Eindhoven.</p><p align="justify">Para ser eleito, o produto precisa oferecer muito mais do que beleza. Funcionalidade, originalidade, respeito, facilidade de uso, inovação e escolha de materiais são igualmente importantes.</p><p align="justify">Considerado como símbolo de bom design industrial, o Good Industrial Design reforça a credibilidade, aumenta a conscientização sobre a importância do design no produto e é tido como um sinal de confiança no poder comercial do produto bem elaborado.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
