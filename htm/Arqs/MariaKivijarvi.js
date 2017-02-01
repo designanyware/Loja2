9e5622fcb0adb93f14e1de1f1672d351223c1e19
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:32 -0200

    Sent by DeployFC at 01/02/2017 20:41:32

---
+++ htm/Arqs/MariaKivijarvi.js
@@ -0,0 +1,12 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/DN_MariaKivijarvi.jpg" width="214" height="214" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Maria Kivijärvi</span></p>');
+document.write('      <p align="justify">Maria Kivijärvi é uma jovem designer Finlandesa cuja paixão é encontrar a forma perfeita. Suas ideais inovadoras são colocadas em prática combinando habilidade e sentimento. Maria olha para o mundo de forma diferente: ela não procura pela beleza, mas a descobre em tudo. Seu trabalho tem recebido inúmeros prêmios e reconhecimento ao redor do mundo, sendo o Cake Server, produzido pela marca Magisso, um de seus mais aclamados produtos.</p></td>');
+document.write('      </p></td>');
+document.write('  </tr>');
+document.write('  </tr>');
+document.write('</table><br />');
