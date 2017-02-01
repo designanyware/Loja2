0bcbb9cfef058537717a88ffbbe6ea8561a64afd
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:14 -0200

    Sent by DeployFC at 01/02/2017 20:41:13

---
+++ htm/Arqs/IfDesignAward.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>')
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_DesignAward.gif" width="214" height="100" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">iF Design Award</span></p>');
+document.write('      <p align="justify">Iniciado em 1953, o iF Design é um dos mais antigos concursos de design do mundo. O prêmio é concedido anualmente, pela instituição independente iF, em três diferentes categorias: design de produto, design de comunicação e design de embalagem.</p><p align="justify">São avaliados critérios como: qualidade do design, acabamento, escolha de materiais, grau de inovação, impacto ambiental, funcionalidade, ergonomia, segurança, desenho universal, entre outros.</p><p align="justify">Reconhecido globalmente como um selo de excelência e qualidade, o iF Design funciona como uma plataforma de produtos inovadores e de design relevante, e ainda como uma importante ferramenta de marketing para as empresas e estúdios de design, que se orgulham em exibi-lo em seus produtos.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
