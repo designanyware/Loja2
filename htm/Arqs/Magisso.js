59f7f9c475cc6781ef81b7652247dcd8f3a23992
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:30 -0200

    Sent by DeployFC at 01/02/2017 20:41:30

---
+++ htm/Arqs/Magisso.js
@@ -0,0 +1,12 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/MA_Magisso.gif" width="214" height="56" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Magisso</span></p><p align="justify">Magisso é uma marca Finlandesa cuja missão é fazer a diferença e procurar problemas para resolver, criando produtos que são únicos em termos de função, design e facilidade de uso. Desenvolvidos a partir de necessidades reais, os produtos Magisso são simples porém inteligentes e chegaram para facilitar nosso dia a dia. </p><p align="justify">Criada em 2006, a empresa vem se destacado internacionalmente com produtos vencedores de alguns dos mais prestigiados prêmios de design do mundo.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
