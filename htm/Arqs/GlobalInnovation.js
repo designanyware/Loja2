2145078f3d68bfe4bfd77e365c016022fc2b5cf4
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:00 -0200

    Sent by DeployFC at 01/02/2017 20:41:00

---
+++ htm/Arqs/GlobalInnovation.js
@@ -0,0 +1,9 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_GlobalInnovation.gif" width="214" height="126" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Global Innovation Award</span></p><p align="justify">Global Innovation Award (gia) é o principal programa de premiação internacional direcionado para a indústria de utensílios domésticos. É organizado e patrocinado pela Associação Internacional de Utilidades domésticas e pela feira “International Home + Housewares Show”, que acontece anualmente em Chicago, EUA.</p><p align="justify">Lançado em 1999, já premiou varejistas de mais de 30 países. A partir de 2011 foi introduzida a categoria design de excelência, passando a cobrir a indústria como um todo, premiando desde varejistas e fabricantes até os designers dos produtos.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
