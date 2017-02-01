0391e961246d4fea89b7a8a4168d66e36e1ee778
designanyware <miriam@designanyware.com.br>
2017-02-01 20:40:29 -0200

    Sent by DeployFC at 01/02/2017 20:40:29

---
+++ htm/Arqs/DavidHolcomb.js
@@ -0,0 +1,14 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/DN_DavidHolcomb.jpg" width="214" height="200" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">David Holcomb</span></p>');
+document.write('     <p align="justify">Conhecido como um Chef comerciante, David Holcomb se interessou por design de produto desde muito jovem. Um dos primeiros sinais de que seguiria essa profissão, foi quando, aos 6 anos de idade, transformou um pedaço de madeira em um abridor de cartas para sua avó. Em 1982 projetou seu primeiro utensílio de cozinha, a máquina de alho, que permitia que alho fresco fosse pressionado e armazenado sem deixar cheiro nas mãos. Sua praticidade e facilidade de uso a transformaram em um grande sucesso. Pouco tempo depois nasceu a marca Chef’n, com David Holcomd como seu CEO. Desde então ele tem criado uma variedade de produtos inovadores e descolados para cozinha, que fazem sucesso ao redor do mundo.');
+document.write('      </p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
