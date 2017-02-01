b9351f7817a505966e92ca3cedc4760f321798eb
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:09 -0200

    Sent by DeployFC at 01/02/2017 20:41:08

---
+++ htm/Arqs/GraemeDavies.js
@@ -0,0 +1,14 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top">&nbsp;</td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Graeme Davies</span></p>');
+document.write('      <p align="justify">Nascido em Joanesburgo e educado na Cidade do Cabo, Graeme Davies se formou engenheiro eletomecânico pela UCT, antes de fazer o mestrado em Design no Royal College ao Arts em Londres.  Antes de regressar para a África do Sul, ficou 6 meses na Alemanha, onde trabalhou com estratégia de marca na Dyson.');
+document.write('      </p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
