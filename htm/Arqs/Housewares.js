ffcc7c75c1f52672efe811044a8b899bbcb67823
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:10 -0200

    Sent by DeployFC at 01/02/2017 20:41:10

---
+++ htm/Arqs/Housewares.js
@@ -0,0 +1,13 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/PM_Houseware.gif" width="214" height="140" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">Housewares Design Award</span></p>');
+document.write('      <p align="justify">Housewares Design é uma competição anual de design de produto focada na indústria de utilidades domésticas, lançada em 2003 pela Homeworld Business, uma das principais publicações sobre utilidades domésticas dos Estados Unidos.</p><p align="justify">O principal objetivo do prêmio é reconhecer a inovação no design como um dos mais importantes motores para o crescimento da indústria de utilidades domésticas. Os produtos inscritos são avaliados por uma comissão julgadora independente, composta por renomados designers e compradores de varejo, num processo gerido pela IHA (International Housewares Association - Associação Internacional de Utilidades domésticas).</p><p align="justify">Os critérios de avaliação incluem: estética, funcionalidade, tecnologia ou materiais inovadores, segurança, sustentabilidade, impacto social, capacidade de atender às necessidades dos consumidores, entre outros.</p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
