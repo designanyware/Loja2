a3f3d291c80475418197ee5aba548645b60b46dd
designanyware <miriam@designanyware.com.br>
2017-02-01 20:41:39 -0200

    Sent by DeployFC at 01/02/2017 20:41:39

---
+++ htm/Arqs/oxotot.js
@@ -0,0 +1,15 @@
+document.write('<br /><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #D7D7D7;padding:10px 10px 10px 10px;">');
+document.write('  <tr>');
+document.write('    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">');
+document.write('  <tr>');
+document.write('    <td width="214" valign="top"><img src="../lojas/00023005/images/MA_OXOtot.gif" width="214" height="95" border="0" /></td>');
+document.write('    <td width="10"><img src="../lojas/00023005/images/Trans.gif" width="10" height="10" border="0" /></td>');
+document.write('    <td valign="top"><p align="justify"><span class="VermM_">OXO tot</span></p>');
+document.write('      <p align="justify"> Fundada em 1990 sob o conceito do desenho universal, a empresa americana OXO tem como objetivo principal criar produtos que possam ser utilizados pelo maior número de pessoas possível, considerando suas diferentes características e necessiades. Este objetivo força a empresa a identificar os problemas e ineficiências nos produtos existentes, para poder aprimorá-los, tanto em termos de ergonomia quando de desempenho.</p>');
+document.write('      <p align="justify"> Sua linha infantil tot foi desenvolvida para crescer junto com a criança, podendo ser utilizada por muitos anos, durante todos os estágios de crescimento. Todos os produtos são intuitivos, fáceis de usar e produzidos com materiais de alta qualidade, amplamente testados.');
+document.write('      </p></td>');
+document.write('  </tr>');
+document.write('</table>');
+document.write('</td>');
+document.write('  </tr>');
+document.write('</table><br />');
